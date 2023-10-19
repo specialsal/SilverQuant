3d069b078b049c2d87f901dc3b5d9dc81944befd
Junchao <silver6wings@163.com>
2023-10-20 01:02:35 +0800

    Update utils_cache.py

--- tools/utils_cache.py
+++ tools/utils_cache.py
@@ -8,15 +8,16 @@ OPEN_DAY_CACHE_PATH = '_cache/_open_day_list.csv'
 open_day_cache = {}
 
 
-def check_today_is_open_day_by_df(df, today):
+def check_today_is_open_day_by_df(df: pd.DataFrame, today: str):
     df = df[df['is_open'] == 1]
-    is_today_open_day = today in df['cal_date'].values
+    today_int = int(today)
+    is_today_open_day = today_int in df['cal_date'].values
     open_day_cache[today] = is_today_open_day
     return is_today_open_day
 
 
 def check_today_is_open_day(now: datetime.datetime):
-    today = int(now.strftime("%Y%m%d"))
+    today = now.strftime("%Y%m%d")
 
     # 内存缓存
     if today in open_day_cache.keys():
@@ -26,17 +27,21 @@ def check_today_is_open_day(now: datetime.datetime):
     if os.path.exists(OPEN_DAY_CACHE_PATH):  # 文件缓存存在
         df = pd.read_csv(OPEN_DAY_CACHE_PATH)
         if today <= df['cal_date'].max():  # 文件缓存未过期
-            return check_today_is_open_day_by_df(df, today)
+            open_day_cache[today] = check_today_is_open_day_by_df(df, today)
+            return open_day_cache[today]
 
     # 网络缓存
-    curr_year = datetime.datetime.now().year
-    next_year = curr_year + 1
+    curr_year = now.year
 
     pro = get_tushare_pro(0)
-    df = pro.trade_cal(exchange='', start_date=str(curr_year) + '0101', end_date=str(next_year) + '1231')
+    df = pro.trade_cal(
+        exchange='',
+        start_date=str(curr_year - 1) + '0101',
+        end_date=str(curr_year + 1) + '1231',
+    )
     df.to_csv(OPEN_DAY_CACHE_PATH)
-
-    return check_today_is_open_day_by_df(df, today)
+    open_day_cache[today] = check_today_is_open_day_by_df(df, today)
+    return open_day_cache[today]
 
 
 def get_all_historical_symbols():
