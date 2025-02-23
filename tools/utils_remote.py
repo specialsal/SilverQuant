435e60094bf23424e2e0f9d7e9068495dc072079
dominicx <dominic@live.cn>
2025-02-23 16:46:05 +0800

    修正未加入函数import的错误

--- tools/utils_remote.py
+++ tools/utils_remote.py
@@ -5,7 +5,7 @@ from typing import Optional
 
 import akshare as ak
 import pywencai
-from tools.utils_basic import is_symbol, code_to_symbol
+from tools.utils_basic import is_symbol, is_stock, code_to_symbol
 
 
 def get_wencai_codes(queries: list[str]) -> list[str]:
